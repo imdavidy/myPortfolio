8ec4dc18497a1d63514acf58e82e6f6ee5c3be28
David Yoon <imdavidy@gmail.com>
2018-01-09 17:02:19 -0500

    setup server

---
+++ server/index.js
@@ -0,0 +1,23 @@
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const {resolve} = require('path')
+
+const app = express()
+
+app.use(express.static(resolve(__dirname, '..', 'public')))
+app.use('/api', (req, res, next) => {
+  res.send('api reached')
+})
+app.use('*', (req, res, next) => {
+  res.send('welcome!')
+})
+
+app.use((err, req, res, next) => {
+  res.send(err);
+})
+
+app.listen(1337,
+  ()=>{
+    console.log("connected to server")
+  })
